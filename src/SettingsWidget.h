2f896788b7ec26e27864864891dfff348fbaca7b
Thomas <t.kroes@lumc.nl>
2020-01-17 11:51:12 +0100

    Documented the settings widget class

--- src/SettingsWidget.h
+++ src/SettingsWidget.h
@@ -29,15 +29,56 @@ public:
 	~SettingsWidget();
 
 private:
+	/** 
+	 * Triggered when data set names changed
+	 * @param datasetNames Data set names
+	 */
 	void onDatasetNamesChanged(const QStringList& datasetNames);
+
+	/** 
+	 * Triggered when the current data set changed
+	 * @param currentDataset Current data set
+	 */
 	void onCurrentDatasetChanged(const QString& currentDataset);
+
+	/** 
+	 * Triggered when image names changed
+	 * @param imageNames Image names
+	 */
 	void onImageNamesChanged(const QStringList& imageNames);
+
+	/** 
+	 * Triggered when the current image changed
+	 * @param currentImage Current image index
+	 */
 	void onCurrentImageChanged(const int& currentImage);
+
+	/** 
+	 * Triggered when dimension names changed
+	  * @param dimensionNames Dimension names
+	 */
 	void onDimensionNamesChanged(const QStringList& dimensionNames);
+
+	/** 
+	 * Triggered when current dimension changed
+	 * @param currentDimension Current dimension index
+	 */
 	void onCurrentDimensionChanged(const int& currentDimension);
+
+	/**
+	 * Triggered when average images changed
+	 * @param averageImages Whether sequence images should be averaged
+	 */
 	void onAverageImagesChanged(const bool& averageImages);
+
+	/** 
+	 * Triggered when the selection image changed
+	 * @param selectionImage Selection image
+	 * @param selectionBounds Two-dimensional boundaries of the selected pixel in the selection image
+	 */
 	void onSelectionImageChanged(std::shared_ptr<QImage> selectionImage, const QRect& selectionBounds);
 	
+	/** Update the selection opacity user interface */
 	void updateSelectionOpacityUI();
 
 private:
