c669dd09dcbd162f3409124cdd552a2e66d77a8f
Thomas <t.kroes@lumc.nl>
2020-01-17 12:05:03 +0100

    Documented the quad renderer class

--- src/QuadRenderer.h
+++ src/QuadRenderer.h
@@ -33,18 +33,31 @@ public:
 	void render() override;
 
 public:
+	/** Returns the size of the quad */
 	QSize size() const;
+
+	/**
+	 * Sets the size of the image quad
+	 * @param size Size of the image quad
+	 */
 	void setSize(const QSize& size);
 
 protected:
+	/** Create the necessary OpenGL shader programs */
 	void createShaderPrograms() override  = 0;
+
+	/** Create the necessary OpenGL Vertex Buffer Objects (VBO) */
 	void createVBOs() override;
+
+	/** Create the necessary OpenGL Vertex Array Objects (VAO) */
 	void createVAOs() override;
 
 private:
+	/** Create the quad using OpenGL*/
 	void createQuad();
 
 signals:
+	/** Triggered when the quad size has changed */
 	void sizeChanged(const QSize& size);
 
 protected:
