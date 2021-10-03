95ab799fb4717a48c08dcf52f41ae48c9a32b56d
Thomas Kroes <t.kroes@lumc.nl>
2021-10-03 16:45:03 +0200

    Remove redundant widget

--- src/SelectionAction.h
+++ src/SelectionAction.h
@@ -24,37 +24,6 @@ class SelectionAction : public PixelSelectionAction
 {
 public:
 
-    /** Widget class for settings action */
-    class Widget : public WidgetActionWidget
-    {
-    protected:
-
-        /**
-         * Constructor
-         * @param parent Pointer to parent widget
-         * @param selectionAction Pointer to selection action
-         * @param state State of the widget
-         */
-        Widget(QWidget* parent, SelectionAction* selectionAction, const WidgetActionWidget::State& state);
-
-    protected:
-        friend class SelectionAction;
-    };
-
-protected:
-
-    /**
-     * Get widget representation of the selection action
-     * @param parent Pointer to parent widget
-     * @param widgetFlags Widget flags for the configuration of the widget (type)
-     * @param state State of the widget (for stateful widgets)
-     */
-    QWidget* getWidget(QWidget* parent, const std::int32_t& widgetFlags, const WidgetActionWidget::State& state = WidgetActionWidget::State::Standard) override {
-        return new Widget(parent, this, state);
-    };
-
-public:
-
     /**
      * Constructor
      * @param layerAction Reference to layer action
