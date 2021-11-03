33a36fec327962cdd6b77ce77350fe8c3c13931b
B. van Lew <b.van_lew@lumc.nl>
2021-11-03 09:28:07 +0100

    Use branch derived hdps-core version in CI

--- conanfile.py
+++ conanfile.py
@@ -14,37 +14,42 @@ class ImageViewerPluginConan(ConanFile):
     versioninfo based on the branch naming convention
     as described in https://github.com/hdps/core/wiki/Branch-naming-rules
     """
+
     name = "ImageViewerPlugin"
-    description = "A plugin for viewing image data in the high-dimensional plugin system (HDPS)."
+    description = (
+        "A plugin for viewing image data in the high-dimensional plugin system (HDPS)."
+    )
     topics = ("hdps", "plugin", "image data", "viewing")
     url = "https://github.com/hdps/ImageViewerPlugin"
-    author = "B. van Lew b.van_lew@lumc.nl"   # conan recipe author
+    author = "B. van Lew b.van_lew@lumc.nl"  # conan recipe author
     license = "MIT"
 
     short_paths = True
-    generators = ("cmake")
+    generators = "cmake"
 
     # Options may need to change depending on the packaged library
     settings = {"os": None, "build_type": None, "compiler": None, "arch": None}
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": True, "fPIC": True}
 
-    requires = (
-        "qt/5.15.1@lkeb/stable",
-        "hdps-core/latest@lkeb/stable"
-    )
+    requires = "qt/5.15.1@lkeb/stable"
 
     scm = {
         "type": "git",
         "subfolder": "hdps/ImageViewerPlugin",
         "url": "auto",
-        "revision": "auto"
+        "revision": "auto",
     }
 
     def set_version(self):
         # Assign a version from the branch name
         branch_info = PluginBranchInfo(self.recipe_folder)
         self.version = branch_info.version
+
+    def requirements(self):
+        branch_info = PluginBranchInfo(self.__get_git_path())
+        print(f"Core requirement {branch_info.core_requirement}")
+        self.requires(branch_info.core_requirement)
 
     # Remove runtime and use always default (MD/MDd)
     def configure(self):
@@ -56,13 +61,13 @@ class ImageViewerPluginConan(ConanFile):
         pass
 
     def config_options(self):
-        if self.settings.os == 'Windows':
+        if self.settings.os == "Windows":
             del self.options.fPIC
 
     def _configure_cmake(self, build_type):
         # locate Qt root to allow find_package to work
         qtpath = pathlib.Path(self.deps_cpp_info["qt"].rootpath)
-        qt_root = str(list(qtpath.glob('**/Qt5Config.cmake'))[0].parents[3])
+        qt_root = str(list(qtpath.glob("**/Qt5Config.cmake"))[0].parents[3])
         print("Qt root ", qt_root)
 
         cmake = CMake(self, build_type=build_type)
@@ -76,41 +81,55 @@ class ImageViewerPluginConan(ConanFile):
         return cmake
 
     def build(self):
-        print('Build OS is : ', self.settings.os)
+        print("Build OS is : ", self.settings.os)
         # If the user has no preference in HDPS_INSTALL_DIR simply set the install dir
-        if not os.environ.get('HDPS_INSTALL_DIR', None):
-            os.environ['HDPS_INSTALL_DIR'] = os.path.join(self.build_folder, "install")
-        print('HDPS_INSTALL_DIR: ', os.environ['HDPS_INSTALL_DIR'])
-        self.install_dir = os.environ['HDPS_INSTALL_DIR']
+        if not os.environ.get("HDPS_INSTALL_DIR", None):
+            os.environ["HDPS_INSTALL_DIR"] = os.path.join(self.build_folder, "install")
+        print("HDPS_INSTALL_DIR: ", os.environ["HDPS_INSTALL_DIR"])
+        self.install_dir = os.environ["HDPS_INSTALL_DIR"]
 
         # The ImageViewerPlugin build expects the HDPS package to be in this install dir
         hdps_pkg_root = self.deps_cpp_info["hdps-core"].rootpath
         print("Install dir type: ", self.install_dir)
         shutil.copytree(hdps_pkg_root, self.install_dir)
 
-        cmake_debug = self._configure_cmake('Debug')
+        cmake_debug = self._configure_cmake("Debug")
         cmake_debug.build()
 
-        cmake_release = self._configure_cmake('Release')
+        cmake_release = self._configure_cmake("Release")
         cmake_release.build()
 
     def package(self):
         package_dir = os.path.join(self.build_folder, "package")
-        print('Packaging install dir: ', package_dir)
-        subprocess.run(["cmake",
-                        "--install", self.build_folder,
-                        "--config", "Debug",
-                        "--prefix", os.path.join(package_dir, "Debug")])
-        subprocess.run(["cmake",
-                        "--install", self.build_folder,
-                        "--config", "Release",
-                        "--prefix", os.path.join(package_dir, "Release")])
+        print("Packaging install dir: ", package_dir)
+        subprocess.run(
+            [
+                "cmake",
+                "--install",
+                self.build_folder,
+                "--config",
+                "Debug",
+                "--prefix",
+                os.path.join(package_dir, "Debug"),
+            ]
+        )
+        subprocess.run(
+            [
+                "cmake",
+                "--install",
+                self.build_folder,
+                "--config",
+                "Release",
+                "--prefix",
+                os.path.join(package_dir, "Release"),
+            ]
+        )
         self.copy(pattern="*", src=package_dir)
 
     def package_info(self):
-        self.cpp_info.debug.libdirs = ['Debug/lib']
-        self.cpp_info.debug.bindirs = ['Debug/Plugins', 'Debug']
-        self.cpp_info.debug.includedirs = ['Debug/include', 'Debug']
-        self.cpp_info.release.libdirs = ['Release/lib']
-        self.cpp_info.release.bindirs = ['Release/Plugins', 'Release']
-        self.cpp_info.release.includedirs = ['Release/include', 'Release']
+        self.cpp_info.debug.libdirs = ["Debug/lib"]
+        self.cpp_info.debug.bindirs = ["Debug/Plugins", "Debug"]
+        self.cpp_info.debug.includedirs = ["Debug/include", "Debug"]
+        self.cpp_info.release.libdirs = ["Release/lib"]
+        self.cpp_info.release.bindirs = ["Release/Plugins", "Release"]
+        self.cpp_info.release.includedirs = ["Release/include", "Release"]
