e734fd60dbfaac133105b44b00b6f67a49ad2dbf
Thomas Kroes <t.kroes@lumc.nl>
2023-12-10 18:57:24 +0100

    Center modal tasks dialog w.r.t. main window upon show()

--- src/ImageViewerPlugin.cpp
+++ src/ImageViewerPlugin.cpp
@@ -34,7 +34,7 @@ ImageViewerPlugin::ImageViewerPlugin(mv::plugin::PluginFactory* factory) :
     setObjectName("Images");
 
     getWidget().setContextMenuPolicy(Qt::CustomContextMenu);
-    getWidget().setFocusPolicy(Qt::ClickFocus);
+    //getWidget().setFocusPolicy(Qt::ClickFocus);
 
     _interactionToolbarAction.initialize(this);
 }
@@ -78,7 +78,9 @@ void ImageViewerPlugin::init()
             dropRegions << new DropWidget::DropRegion(this, "Images", QString("Add an image layer for %1").arg(datasetGuiName), "images", true, [this, datasetGuiName, dataset]() {
                 try
                 {
-                    addDataset(dataset);
+                    getWidget().setFocus(Qt::OtherFocusReason);
+
+                    loadData({ dataset });
                 }
                 catch (std::exception& e)
                 {
@@ -330,6 +332,8 @@ void ImageViewerPlugin::addDataset(const Dataset<Images>& dataset)
         layer->scaleToFit(_imageViewerWidget.getWorldBoundingRectangle(false));
 
     _layersModel.addLayer(layer);
+
+    getImageViewerWidget().update();
 }
 
 void ImageViewerPlugin::onLayerSelectionChanged()
